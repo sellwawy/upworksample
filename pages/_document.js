18f087427201b441814d0d9a2d73e6328323e04e
Mohamed Mahmoud <96956614+sellwawy@users.noreply.github.com>
2024-10-09 21:03:25 +0000

    Edit on _document and 404 pages

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document(props) {
   return (
-    <Html className="h-full bg-gray-100">
+    <Html className="bg-gray-100">
       <Head />
-      <body className="h-full">
+      <body>
         <Main />
         <NextScript />
       </body>
